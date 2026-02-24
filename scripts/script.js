0ffda7928e975a06e836b052734c3d9070eef858
mostafagamalbisher <mostafagamalbisher@outlook.com>
2026-02-25 00:01:25 +0300

    repo

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+const removeButton = document.querySelector(`.find-out-link`);
+removeButton.remove();
+
+const modifyPicWidth = document.querySelector(`.bio__pic`);
+modifyPicWidth.style.width = "36rem";
+
+const modifySkills = (skillsTitle, years) => {};
