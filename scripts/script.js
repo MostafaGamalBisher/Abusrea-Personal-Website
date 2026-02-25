ec26661f1f7e69defc14f3000872897204dec98f
MostafaGamalBisher <mostafagamalbisher@outlook.com>
2026-02-25 03:52:51 +0300

    Implement modifySkills function to update skills items in the DOM

--- scripts/script.js
+++ scripts/script.js
@@ -4,4 +4,13 @@ removeButton.remove();
 const modifyPicWidth = document.querySelector(`.bio__pic`);
 modifyPicWidth.style.width = "36rem";
 
-const modifySkills = (skillsTitle, years) => {};
+const modifySkills = (parent, selector1, newText1, selector2, newText2) => {
+  const element1 = parent.querySelector(selector1);
+  const element2 = parent.querySelector(selector2);
+  if (element1) element1.textContent = newText1;
+  if (element2) element2.textContent = newText2;
+};
+
+const skillsItem = document.querySelectorAll(`.skills__item`);
+modifySkills(skillsItem[4], "h2", "Node.js");
+modifySkills(skillsItem[5], "h2", "MongoDB", "span", "10 Years");
