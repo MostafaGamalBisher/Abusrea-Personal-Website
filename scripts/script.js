6f4cc10acb311b2549ef4ef55e30a54035317421
mostafagamalbisher <mostafagamalbisher@outlook.com>
2026-02-25 12:12:49 +0300

    Add new skills item for Typescript to the skills list

--- scripts/script.js
+++ scripts/script.js
@@ -14,3 +14,14 @@ const modifySkills = (parent, selector1, newText1, selector2, newText2) => {
 const skillsItem = document.querySelectorAll(`.skills__item`);
 modifySkills(skillsItem[4], "h2", "Node.js");
 modifySkills(skillsItem[5], "h2", "MongoDB", "span", "10 Years");
+
+const listElement = `
+          <li class="skills__item">
+            <h2 class="skills__title">Typescript</h2>
+            <span class="skills__years">2 years</span>
+          </li>
+`;
+document.createRange().createContextualFragment(listElement);
+
+const skillsList = document.querySelectorAll(".skills__item");
+skillsList[3].insertAdjacentHTML("beforebegin", listElement);
